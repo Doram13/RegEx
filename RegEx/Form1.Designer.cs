2745775c878fe902c67ef26d7a2a6a784cdfc55a
Doram13 <1991doram@gmail.com>
2019-03-24 13:48:48 +0100

    First part completed

--- RegEx/Form1.Designer.cs
+++ RegEx/Form1.Designer.cs
@@ -28,13 +28,110 @@ namespace RegEx
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Name = new System.Windows.Forms.Label();
+            this.Phone = new System.Windows.Forms.Label();
+            this.email = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.Name = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.Name.Location = new System.Drawing.Point(0, 0);
+            this.Name.Name = "label1";
+            this.Name.Size = new System.Drawing.Size(100, 23);
+            this.Name.TabIndex = 7;
+            // 
+            // Phone
+            // 
+            this.Phone.AutoSize = true;
+            this.Phone.Location = new System.Drawing.Point(48, 80);
+            this.Phone.Name = "Phone";
+            this.Phone.Size = new System.Drawing.Size(57, 17);
+            this.Phone.TabIndex = 1;
+            this.Phone.Text = "Phone: ";
+            // 
+            // email
+            // 
+            this.email.AutoSize = true;
+            this.email.Location = new System.Drawing.Point(51, 122);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(55, 17);
+            this.email.TabIndex = 2;
+            this.email.Text = "E-mail: ";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(121, 37);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(231, 22);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(121, 80);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(231, 22);
+            this.txtPhone.TabIndex = 4;
+
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(121, 116);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(231, 22);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(299, 160);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Location = new System.Drawing.Point(51, 37);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(53, 17);
+            this.Name.TabIndex = 8;
+            this.Name.Text = "Name: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(404, 195);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.Phone);
+            this.Controls.Add(this.Name);
+            this.Text = "Input Validator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private new System.Windows.Forms.Label Name;
+        private System.Windows.Forms.Label Phone;
+        private System.Windows.Forms.Label email;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnSave;
     }
 }
 
--- RegEx/Form1.cs
+++ RegEx/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,42 @@ namespace RegEx
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            //the following conditions are for the Invalid input! 
+            if (NameAlphabeticCheck())
+            { 
+                MessageBox.Show("The name is invalid! Only alphabetical characters are allowed.");
+                return;
+            }
+            if (PhoneDigitalCheck())
+            { 
+                MessageBox.Show("The phone number is invalid! Only numbers are allowed.");
+                return;
+            }
+            if (EmailCheck())
+            {
+                MessageBox.Show("The email adress is invalid! Only usual adresses like 'some1thing@someserver.something' are allowed.");
+                return;
+            }
+            MessageBox.Show("Congrats, you input is valid!");
+        }
+
+        private bool NameAlphabeticCheck()
+        {
+            return !Regex.IsMatch(txtName.Text, @"^([A-Za-z]+\s*)+$");
+        }
+
+        private bool PhoneDigitalCheck()
+        {
+            return !Regex.IsMatch(txtPhone.Text, @"^[0-9]+$");
+        }
+
+        private bool EmailCheck()
+        {
+            return !Regex.IsMatch(txtEmail.Text, @"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$");
         }
     }
 }
